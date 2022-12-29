c98317c935ea22f00d07bab28d2a32bda6382bdc
Polpy <emailparaoedu@gmail.com>
2022-12-29 23:22:06 +0000

    Hugo Fix, new BG

--- everything-fi/src/containers/Home/Home.jsx
+++ everything-fi/src/containers/Home/Home.jsx
@@ -5,13 +5,14 @@ import './Home.css'
 function Home() {
     return (
         <div className="Home">
-            <video src={images.bg_video} id='video' loop autoPlay muted alt="background video"/>
-
             <div className="home-logo">
                 <img src={images.logo} alt="everything-fi logo" />
             </div>
             <div className="home-sub-heading">
                 literacia financeira simplificada
+            </div>
+            <div className='wrapperGradient'>
+                <video src={images.bg_video} id='video' loop autoPlay muted alt="background video"/>
             </div>
         </div>
     )
