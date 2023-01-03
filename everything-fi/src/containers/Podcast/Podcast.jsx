c744dd5377e233ae3642f47032447813dfa8b06e
Polpy <emailparaoedu@gmail.com>
2023-01-03 18:50:11 +0000

    podcast column improvements

--- everything-fi/src/containers/Podcast/Podcast.jsx
+++ everything-fi/src/containers/Podcast/Podcast.jsx
@@ -7,7 +7,7 @@ const Podcast = () => {
         <div className="app_podcast">
             <h1 >HIGHLIGHTS PODCAST</h1>
             <h2>BREVEMENTE...</h2>
-            <p>Mantenha-se atualizado! Visite as nossas redes sociais e fique a par de tudo o que estamos a planear para si. Siga-nos e aprenda connosco a atingir a estabilidade financeira que tanto procura!</p>
+            <span>Mantenha-se atualizado! Visite as nossas redes sociais e fique a par de tudo o que estamos a planear para si. Siga-nos e aprenda connosco a atingir a estabilidade financeira que tanto procura!</span>
 
         </div>
 
