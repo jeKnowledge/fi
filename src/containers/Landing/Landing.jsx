0ed63d4bdfcdb0a969b6a6a2aa19b3d36c061997
Polpedu <emailparaoedu@gmail.com>
2023-01-19 02:08:12 +0000

    REFACTOR & finalizing

---
+++ src/containers/Landing/Landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar, Footer } from '../../components'
+import { Home, About, Podcast, Temas} from '../'
+import './Landing.css'
+
+function Landing() {
+  return (
+    <div className="landing">
+      <Home />
+      <div className="sticky">
+        <Navbar />
+      </div>
+      <About />
+      <Temas />
+      <Podcast />
+      <Footer />
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/containers/Podcast/Podcast.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Podcast.css'
+import Faq from '../Faq/Faq';
+
+const Podcast = () => {
+
+    return (
+        <>
+            <div className="app_podcast">
+                <h2>HIGHLIGHTS PODCAST</h2>
+                <span className='color_g_brev'>BREVEMENTE...</span>
+                <span>Mantenha-se atualizado! Visite as nossas redes sociais e fique a par de tudo o que estamos a planear para si. Siga-nos e aprenda connosco a atingir a estabilidade financeira que tanto procura!</span>
+                <Faq />
+            </div>
+            <div className='bg-gradientfooter'></div>
+        </>
+    )
+}
+
+export default Podcast;
