bf70f577d29d4381e3f4a638bdbd03b63c2c49d5
miguelopesantana <84048244+miguelopesantana@users.noreply.github.com>
2023-01-20 03:23:14 +0000

    Footer minor fixes and changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
                 </div>
                 <div className="app_footer_logos">
                     <a href={"https://www.instagram.com/_everything.fi_/"} rel="noreferrer" ><img src={images.insta} alt="insta logo"></img></a>
-                    <a href={"/#"}><img src={images.linkedin} alt="linkedin logo"></img></a>
+                    <a href={"https://www.linkedin.com/company/everythingfi/"}><img src={images.linkedin} alt="linkedin logo"></img></a>
                     <a href={"/#"}><img src={images.spotify} alt="spotify logo"></img></a>
                     <a href={"/#"}><img src={images.tiktok} alt="tiktok logo" /></a>
                     <a href={"/#"}><img src={images.youtube} alt="youtube logo" /></a>
