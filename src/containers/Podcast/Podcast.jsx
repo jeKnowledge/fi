14751224aafceb80ffb4fc9414be5c3e8253437a
Polpy <emailparaoedu@gmail.com>
2023-01-21 14:45:34 +0000

    build

--- src/containers/Podcast/Podcast.jsx
+++ src/containers/Podcast/Podcast.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import './Podcast.css'
-import Faq from '../Faq/Faq';
 import Fade from 'react-reveal/Fade';
 
 const Podcast = () => {
 
     return (
         <section className="app_podcast">
-            <Fade delay={500}>
+            <Fade delay={400}>
                 <h1>HIGHLIGHTS PODCAST</h1>
                 <span className='color_g_brev'>BREVEMENTE...</span>
                 <span>Mantenha-se atualizado! Visite as nossas redes sociais e fique a par de tudo o que estamos a planear para si. Siga-nos e aprenda connosco a atingir a estabilidade financeira que tanto procura!</span>
