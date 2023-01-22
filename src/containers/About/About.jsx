b0bf762ff1b3a70272a105e036ec23aa9257585e
miguelopesantana <84048244+miguelopesantana@users.noreply.github.com>
2023-01-22 02:53:12 +0000

    Temas twicking

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -7,7 +7,7 @@ function About() {
     <section className='About'>
         <div className="about-text">
           <h3>
-          Somos uma comunidade com o objetivo de fomentar a literacia financeira e focada difundir conhecimento de indivíduos para tomarem decisões financeiras informadas. O nosso evento e podcast visam fornecer conselhos práticos e pragmáticos sobre uma vasta gama de tópicos, todos com o obejetivo de te ajudar a tomar decisões financeiras informadas. Junta-te a nós e aprende como controlar as tuas finanças e construir um futuro economicamente seguro!
+          Somos uma comunidade com o objetivo de fomentar a literacia financeira e focada em difundir conhecimento acerca de finanças pessoais. O nosso evento e podcast visam fornecer conselhos práticos e pragmáticos sobre uma vasta gama de tópicos, todos com o obejetivo de te ajudar a tomar decisões financeiras informadas. Junta-te a nós e aprende como controlar as tuas finanças e construir um futuro economicamente seguro!
           </h3>
         </div>
     </section>
