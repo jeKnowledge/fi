0240c3c3c22047f0ce52a2c0ab02861ba6f3e81d
NoxCoimbra <pedrorcoimb@gmail.com>
2023-02-15 18:39:29 +0000

    partners done

--- src/constants/images.js
+++ src/constants/images.js
@@ -8,6 +8,13 @@ import tiktok from '../assets/tiktok.svg'
 import youtube from '../assets/yt.svg'
 import spotify from '../assets/spotify.svg'
 import bg_video from '../assets/bg_video.mp4'
+import grama_logo from '../assets/grama_icon.svg'
+import loop_logo from '../assets/loop_icon.svg'
+import good_barber_logo from '../assets/good_barber_icon.svg'
+import feedzai_logo from '../assets/feedzai_icon.svg'
+import cisco_logo from '../assets/cisco_icon.svg'
+import big_jek_logo from '../assets/big_jek_icon.svg'
+import noesis_logo from '../assets/noesis_icon.svg'
 
 
 const imgs = {
@@ -21,6 +28,13 @@ const imgs = {
     youtube,
     spotify,
     bg_video,
+    grama_logo,
+    loop_logo,
+    good_barber_logo,
+    feedzai_logo,
+    cisco_logo,
+    big_jek_logo,
+    noesis_logo,
 }
 
 export default imgs;
--- src/containers/Partners/Partners.jsx
+++ src/containers/Partners/Partners.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Partners.css"
-
+import images from '../../constants/images'
 
 const Partners = () => {
 
@@ -9,13 +9,38 @@ const Partners = () => {
             <h1>PARCERIAS E PATROCINIOS</h1>
             <div className="app_partners_pack">
                 <h1>PACK 1</h1>
+                <div className="app_partners_pack_logos">
+                <a href={""} rel="noreferrer" ><img src={images.loop_logo} alt="loop logo"></img></a>
+                <a href={"https://www.linkedin.com/company/everythingfi/"}><img src={images.big_jek_logo} alt="jek logo"></img></a>
+                </div>
+
+
             </div>
             <div className="app_partners_pack">
                 <h1>PACK 2</h1>
+                <div className="app_partners_pack_logos">
+                <a href={""} rel="noreferrer" ><img src={images.feedzai_logo} alt="feedzai logo"></img></a>
+                <a href={""}><img src={images.noesis_logo} alt="jek logo"></img></a>
+                <a href={""} rel="noreferrer" ><img src={images.good_barber_logo} alt="good_barber logo"></img></a>
+                </div>
             </div>
             <div className="app_partners_pack">
                 <h1>PACK 3</h1>
+                <div className="app_partners_pack_logos">
+                <a href={""} rel="noreferrer" ><img src={images.cisco_logo} alt="cisco logo"></img></a>
+                <a href={""}><img src={images.grama_logo} alt="grama logo"></img></a>
+                </div>
+                </div>
+            <div className = "app_partners_pack">
+                <h1> PODCAST</h1>
+                <div className="app_partners_pack_logos">
+                <a href={""} rel="noreferrer" ><img src={images.feedzai_logo} alt="feedzai logo"></img></a>
+                <a href={""}><img src={images.noesis_logo} alt="jek logo"></img></a>
+                <a href={""} rel="noreferrer" ><img src={images.cisco_logo} alt="cisco logo"></img></a>
+                <a href={""}><img src={images.grama_logo} alt="grama logo"></img></a>
+                </div>
             </div>
+            
         </div>
     )
 
