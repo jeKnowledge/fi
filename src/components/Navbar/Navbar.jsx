bbc75260923b5b72552cfda57504c35b827535a3
iTzHuGo <hugosbarros@gmail.com>
2023-03-13 19:33:36 +0000

    nav without background, no transition animation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import images from '../../constants';
 
 import './Navbar.css'
 
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleListVisible = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+  const handleIconClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="sticky">
-        <nav className="navbar">
+      <nav className="navbar-gen">
+        <div className="navbar">
           <div className="navbar-logo">
             <img src={images.icon} alt="navbar_icon" />
           </div>
-          {/* <div className="navbar-pt-en">
-            <a href='/#' style={{ fontWeight: 500, borderBottom: "1px solid white" }}>PT</a>
-            <a href='/#'>EN</a>
-          </div> */}
-        </nav>
+          <div id='nav-icon3' className={`icon ${isOpen ? 'open' : ''}`} onClick={() => { handleIconClick(); handleListVisible() }}>
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <div />
+        </div>
+
+        {toggleMenu && (
+          <ul className="navbar-links">
+            <li><a href="/#">Parceiros</a></li>
+            <li><a href="/#">Podcast</a></li>
+            <li><a href="/#">Evento</a></li>
+            <li><a href="/#">Equipa</a></li>
+            <li><a href="/#">Contactos</a></li>
+          </ul>
+        )}
+      </nav>
     </div>
   );
-};
+}
 
 export default Navbar
