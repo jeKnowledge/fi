27410e9e1621fafe753308268779bc460f283a3b
iTzHuGo <hugosbarros@gmail.com>
2023-03-25 22:20:53 +0000

    navbar adicionada

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,48 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import { images } from '../../constants';
+import React, { useState } from 'react'
+import images from '../../constants';
+
 import './Navbar.css'
 
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleListVisible = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
+  const handleIconClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={images.icon} alt="navbar_icon" />
-      </div>
-      <div className="navbar-pt-en">
-        <a style={{ fontWeight: 500, borderBottom: "1px solid white" }}>PT</a>
-        <a>EN</a>
-      </div>
-    </nav>
+    <div className="sticky">
+      <nav className="navbar-gen">
+        <div className="navbar">
+          <div className="navbar-logo">
+            <img src={images.icon} alt="navbar_icon" />
+          </div>
+          <div id='nav-icon3' className={`icon ${isOpen ? 'open' : ''}`} onClick={() => { handleIconClick(); handleListVisible() }}>
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <div />
+        </div>
+
+        {toggleMenu && (
+          <ul className="navbar-links">
+            <li><a href="/#">Parceiros</a></li>
+            <li><a href="/#">Podcast</a></li>
+            <li><a href="/#">Evento</a></li>
+            <li><a href="/#">Equipa</a></li>
+            <li><a href="/#">Contactos</a></li>
+          </ul>
+        )}
+      </nav>
+    </div>
   );
-};
+}
 
 export default Navbar
